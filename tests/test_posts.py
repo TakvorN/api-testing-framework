602eca2c6b7a1a8531be194a2c68939b933c5b9e
TakvorN <t.k.nahabedyan@gmail.com>
2026-05-23 03:44:39 +0300

    test: add parametrized GET endpoint tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,7 @@
-def test_get_existing_post(client):
-    response = client.get("posts/1")
+import pytest
+
+@pytest.mark.parametrize("post_id", [1, 2, 3])
+def test_get_existing_post(client, post_id):
+    response = client.get(f"posts/{post_id}")
 
     assert response.status_code == 200
