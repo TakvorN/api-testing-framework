e75d5db7a6dd41e1df0eae93657cd82e1b9013e3
TakvorN <t.k.nahabedyan@gmail.com>
2026-05-29 04:17:26 +0300

    test: add negative GET endpoint test

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -5,3 +5,9 @@ def test_get_existing_post(client, post_id):
     response = client.get(f"posts/{post_id}")
 
     assert response.status_code == 200
+
+    
+def test_get_nonexistent_post(client):
+    response = client.get("posts/999999")
+
+    assert response.status_code == 404
